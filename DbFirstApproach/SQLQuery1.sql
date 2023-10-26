77ce9491bac1d0d407be7dd04d30c7fcb0a7cc2a
Bhavani9347 <144900026+Bhavani9347@users.noreply.github.com>
2023-10-26 17:44:43 +0530

    4

---
+++ DbFirstApproach/SQLQuery1.sql
@@ -0,0 +1,43 @@
+﻿Create Database OnlineshoppingChain
+go
+Drop database OnlineshoppingChain
+go
+---Customer Table
+Create table Customers (CustomerId int Primary key not null,CustomerName Varchar(20) not null,CustomerAddress Varchar(30),CustomerPhoneno int not null)
+Go
+
+Drop Table Customers
+go
+
+---Vendor Table---
+Create table Vendor(VendorId int Primary Key not null,VendorName varchar(20),VendorAddress varchar(30),	VendorEmail varchar(30) not null)
+go
+
+DROP table Vendor
+go
+
+---Product table---
+Create table Product(ProductId int Primary key not null,ProductName varchar(20) not null,Price float not null,
+VendorId int foreign key References Vendor(VendorId),
+CategoryId int foreign key  References Category(CategoryId));
+go
+
+Drop table Product 
+go
+
+----Order Table-----
+Create table Orders(OrderId int Primary key not null,Amount Float not null,
+CustomerId int foreign key References Customers(CustomerId),
+ProductId int foreign key References Product(ProductId),quantity float not null,Orderdate datetime  not null)
+go
+
+Drop Table Orders
+go
+
+-----Category table-----
+Create table Category(
+CategoryId int not null,CategoryName varchar(30) not null)
+go
+
+Drop table Category
+go
---
+++ DbFirstApproach/SQLQuery2.sql
@@ -0,0 +1,32 @@
+﻿Create Database OnlineshoppingChain
+go
+
+---Customer table-----
+Create table Customers (CustomerId int Primary key not null,CustomerName Varchar(20) not null,CustomerAddress Varchar(30),CustomerPhoneno int not null)
+Go
+
+---Vendor Table---
+Create table Vendor(VendorId int Primary Key not null,VendorName varchar(20),VendorAddress varchar(30),	VendorEmail varchar(30) not null)
+go
+
+
+-----Category table-----
+Create table Category(
+CategoryId int Primary key not null,CategoryName varchar(30) not null)
+go
+
+---Drop table category
+Drop table Category
+go
+
+---Product table---
+Create table Product(ProductId int Primary key not null,ProductName varchar(20) not null,Price float not null,
+VendorId int foreign key References Vendor(VendorId),
+CategoryId int foreign key  References Category(CategoryId));
+go
+
+----Order Table-----
+Create table Orders(OrderId int Primary key not null,Amount Float not null,
+CustomerId int foreign key References Customers(CustomerId),
+ProductId int foreign key References Product(ProductId),quantity float not null,Orderdate datetime  not null)
+go
